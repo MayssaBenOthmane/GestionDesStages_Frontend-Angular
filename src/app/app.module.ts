eb37f4cebe3bdf332bff91a2b35abe5346f43a27
abc <abc@gmail.om>
2023-04-24 21:57:21 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PageprofComponent } from './pageprof/pageprof.component';
+import { StageService } from './stage.service';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { PageetudComponent } from './pageetud/pageetud.component';
+import { PageadminComponent } from './pageadmin/pageadmin.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PageprofComponent,
+    PageetudComponent,
+    PageadminComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
+    
+
+
   ],
-  providers: [],
+  providers: [StageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
